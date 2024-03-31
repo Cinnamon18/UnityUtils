3e64895a81ea445b704a5115ada2fbf4a9277fc1
Cinnamon18 <{11944881}+{Cinnamon18}@users.noreply.github.com>
2024-03-31 15:37:55 -0700

    worldspacebutton

---
+++ WorldspaceButton.cs
@@ -0,0 +1,45 @@
+using System;
+using CinGlobalNamespace;
+using Shapes;
+using UnityEngine;
+
+#pragma warning disable CS8632 // Warning for nullables
+
+namespace UI {
+	[RequireComponent(typeof(Collider2D))]
+	public class WorldspaceButton : MonoBehaviour {
+		[Tooltip("Can be null")]
+		[SerializeField] private ShapeRenderer? buttonBg;
+		public bool IsButtonEnabled = true;
+		public event Action OnClick;
+		public event Action OnClickUp;
+
+		private Color startingButtonColor;
+
+		void Awake() {
+			if(buttonBg != null) {
+				startingButtonColor = buttonBg.Color;
+			}
+		}
+
+		public void SetFocused(bool isFocused) {
+			if(buttonBg == null) {
+				return;
+			}
+
+			buttonBg.Color = isFocused ? PaletteManager.Palette.Focused : startingButtonColor;
+		}
+
+		private void OnMouseDown() {
+			if(IsButtonEnabled) {
+				OnClick?.Invoke();
+			}
+		}
+
+		private void OnMouseUp() {
+			if(IsButtonEnabled) {
+				OnClickUp?.Invoke();
+			}
+		}
+	}
+}
